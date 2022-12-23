6ee90205cca50796a16f084cf772f10ed9fe58ae
Kevin Bandison <kbandison@gmail.com>
2022-12-23 08:10:34 -0500

    added beasts and ability to vote

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,27 @@
 import React from "react";
+import "./hornedBeast.css";
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likes: 0,
+    };
+  }
+
+  upVote = () => {
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
   render() {
     return (
-      <>
+      <div className="beasts">
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageURL}></img>
+        <img src={this.props.imageURL} alt="" onClick={this.upVote}></img>
         <p>{this.props.description}</p>
-      </>
+        <p>Heart Here: {this.state.likes}</p>
+      </div>
     );
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,25 @@
 import React from "react";
 import HornedBeasts from "./HornedBeasts";
+import beasts from "./data";
+
+console.log(beasts);
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div>
-        <h3>Main</h3>
-        <HornedBeasts
-          title="Beast 1"
-          description="Beast Number 1"
-          imageURL="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-        />
-        <HornedBeasts
-          title="Beast 2"
-          description="Beast Number 2"
-          imageURL="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-        />
-      </div>
+      <>
+        <h3>Horned Beasts</h3>
+
+        {beasts.map((b) => (
+          <HornedBeasts
+            key={b._id}
+            className="beasts"
+            title={b.title}
+            description={b.description}
+            imageURL={b.image_url}
+          />
+        ))}
+      </>
     );
   }
 }
